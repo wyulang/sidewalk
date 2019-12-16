b19f6dbd40de64d7d93c7918150e58d86d866969
wyulang <37110078+wyulang@users.noreply.github.com>
2019-12-16 15:14:02 +0800

    update

--- server/index.js
+++ server/index.js
@@ -3,5 +3,21 @@ var app = express();
 const path = require('path');
 app.use(express.static(path.join(__dirname, '../dist')))
 require('./router.js')(app);
-var server = app.listen(6001);
+
+const getIPAdress = () => {
+  var interfaces = require('os').networkInterfaces();
+  for (var devName in interfaces) {
+    var iface = interfaces[devName];
+    for (var i = 0; i < iface.length; i++) {
+      var alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
+    }
+  }
+}
+let ip=getIPAdress();
+var server = app.listen(6001,ip,res=>{
+  console.log(ip)
+});
 
