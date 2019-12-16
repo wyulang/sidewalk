08d6dca6b941816df122740fb3ec2e38f7d98a29
wyulang <37110078+wyulang@users.noreply.github.com>
2019-12-16 14:59:02 +0800

    update

--- server/api/user.js
+++ server/api/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db.js');
-const { GetDateDiff } = require('../lib/dateformat.js');
+const { GetDateDiff } = require('../lib/dateFormat.js');
 const path = require('path');
 const fs = require('fs-extra');
 
